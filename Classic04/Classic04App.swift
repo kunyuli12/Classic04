f9c865952a0a590a051bb442c465c92f393262a6
kunyuli <kunyuli16@gmail.com>
2021-09-14 21:18:33 +0800

    Initial Commit

---
+++ Classic04/Classic04App.swift
@@ -0,0 +1,17 @@
+//
+//  Classic04App.swift
+//  Classic04
+//
+//  Created by 暨大附中4 on 2021/9/14.
+//
+
+import SwiftUI
+
+@main
+struct Classic04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Classic04/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Classic04
+//
+//  Created by 暨大附中4 on 2021/9/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
